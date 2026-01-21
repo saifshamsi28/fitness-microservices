1de268cce2539419e0f6e89bfaf0fd740b7e7046
Toxic-saif <saifshamsi9905@gmail.com>
2026-01-21 18:47:23 +0530

    created web Client config class for inter-service communication

---
+++ activityservice/src/main/java/com/saif/fitness/activityservice/config/WebClientConfig.java
@@ -0,0 +1,23 @@
+package com.saif.fitness.activityservice.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    @LoadBalanced //because of this we can call a service by its registry name so it will automatically assign any available instance of that service
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient userServiceWebClient(WebClient.Builder webclientBuilder){
+        return webclientBuilder
+                .baseUrl("http://USERSERVICE")
+                .build();
+    }
+}
