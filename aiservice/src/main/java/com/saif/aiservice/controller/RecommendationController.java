ba284140d9ecd98e63c6975fe5be815f5001ecff
Toxic-saif <saifshamsi9905@gmail.com>
2026-01-23 15:02:18 +0530

    created endpoints for recommendations

---
+++ aiservice/src/main/java/com/saif/aiservice/controller/RecommendationController.java
@@ -0,0 +1,31 @@
+package com.saif.aiservice.controller;
+
+import com.saif.aiservice.model.Recommendation;
+import com.saif.aiservice.service.RecommendationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recommendations")
+@RequiredArgsConstructor
+public class RecommendationController {
+    private final RecommendationService recommendationService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(@PathVariable String userId){
+        return ResponseEntity.ok(recommendationService.getUserRecommendations(userId));
+    }
+
+    @GetMapping("/activity/{activityId}")
+    public ResponseEntity<Recommendation> getActivityRecommendation(@PathVariable String activityId){
+        return ResponseEntity.ok(recommendationService.getRecommendation(activityId));
+    }
+
+
+}
