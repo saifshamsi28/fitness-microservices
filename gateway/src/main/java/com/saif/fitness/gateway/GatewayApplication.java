bf24ccf7b9eb7e604b1aaffe9e371c76f8bf8b4e
Toxic-saif <saifshamsi9905@gmail.com>
2026-01-30 01:04:15 +0530

    deleted unnecessary logging codes

--- gateway/src/main/java/com/saif/fitness/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/saif/fitness/gateway/GatewayApplication.java
@@ -16,16 +16,4 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
-//	@EventListener(ApplicationReadyEvent.class)
-//	public void printRoutes() {
-//		System.out.println("========================================");
-//		System.out.println("LOADED GATEWAY ROUTES:");
-//		System.out.println("========================================");
-//		routeLocator.getRoutes().subscribe(route ->
-//				System.out.println("Route ID: " + route.getId() +
-//						" | URI: " + route.getUri() +
-//						" | Predicate: " + route.getPredicate()));
-//		System.out.println("========================================");
-//	}
 }
