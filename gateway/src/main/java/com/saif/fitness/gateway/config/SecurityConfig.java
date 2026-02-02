bfd6caf2540246f1a611e624cee31a2a6d6edadd
Toxic-saif <saifshamsi9905@gmail.com>
2026-02-02 19:52:21 +0530

    added necessary dependency for keycloak admin api and added auth path to public check which dont need acess token

--- gateway/src/main/java/com/saif/fitness/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/saif/fitness/gateway/config/SecurityConfig.java
@@ -1,10 +1,13 @@
-package com.saif.fitness.gateway;
+package com.saif.fitness.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -15,8 +18,27 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity httpSecurity){
         return httpSecurity
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/auth/**").permitAll()
+
+                        // Everything else needs authentication
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
                 .build();
     }
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder(
+            @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri:}") String jwkSetUri) {
+
+        // If config server is not available (like during tests), use a default URI
+        String uriToUse = jwkSetUri.isEmpty()
+                ? "http://localhost:8181/realms/fitness-app/protocol/openid-connect/certs"
+                : jwkSetUri;
+
+        return NimbusReactiveJwtDecoder.withJwkSetUri(uriToUse).build();
+    }
+
 }
